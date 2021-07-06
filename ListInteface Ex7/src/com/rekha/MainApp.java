00e55e848866b54b54529e9befe142f1672c8ae6
Shree <Shree@DESKTOP-HLUI74Q>
2021-07-06 23:33:55 +0530

    Comparator Interface

---
+++ ListInteface Ex7/src/com/rekha/MainApp.java
@@ -0,0 +1,52 @@
+package com.rekha;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+
+//import com.rekha.example.Employee;
+
+
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		List <Employee>empList = Arrays.asList(
+				
+				new Employee(193,"Rekha", 19000.00),
+				new Employee(194,"Kisan", 22000.00),
+				new Employee(195,"Kritika", 30000.00),
+				new Employee(196,"Vilas", 34000.00),
+				new Employee(197,"Ashwini", 54000.00)
+				);
+		
+		Collections.sort(empList,new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee e1, Employee e2) {
+				if(e1.empCode > e2.empCode)
+				return 1;
+				
+				else if (e1.empCode > e2.empCode)
+					return -1;
+				
+				else
+					return 0;
+			}
+			
+		          
+		        
+				
+		});
+		
+		System.out.println("Employee list after sorting using Comparator,");
+		for(Employee emp:empList)
+		{
+			System.out.println(emp);
+		}
+		
+
+	}
+
+}
---
+++ ListInteface Ex7/src/com/rekha/example/Employee.java
@@ -0,0 +1,35 @@
+package com.rekha.example;
+
+public class Employee {
+
+	public int empCode;
+	public String empName;
+	public double salary;
+	
+	
+	public Employee()
+	{
+		this.empCode = -1;
+		this.empName = "xyz";
+		this.salary = -1;
+	}
+
+
+	public Employee(int empCode, String empName, double salary) {
+		super();
+		this.empCode = empCode;
+		this.empName = empName;
+		this.salary = salary;
+	}
+
+
+	@Override
+	public String toString() {
+		
+		return String.format("%10d %20s %10.2f", this.empCode, this.empName, this.salary);
+	}
+		
+
+	
+
+}
