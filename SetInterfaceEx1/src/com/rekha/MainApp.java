dbe781fd93d3708629e84754c79666c54c7dcbed
Shree <Shree@DESKTOP-HLUI74Q>
2021-07-08 16:42:31 +0530

    Set Interface Ex1

---
+++ SetInterfaceEx1/src/com/rekha/MainApp.java
@@ -0,0 +1,26 @@
+package com.rekha;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class MainApp {
+
+	
+	public static void main(String[] args) {
+             Set s = new HashSet();
+             
+             s.add(10);
+             s.add(20);
+             s.add(30);
+             s.add(40);
+             s.add(50);
+             s.add(10);
+             s.add(10);
+
+             
+             System.out.println("HashSet Object = " +s);
+             
+
+	}
+
+}
